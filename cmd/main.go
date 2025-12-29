3a76a5268c4d1435f2e8ce047575f9da278e2e4c
Macbook Air <macbookair@MacBook-Air-NIKITA--Macbook.local>
2025-12-29 15:50:41 +0400

    fix bugs

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	banks "PaymentsBot/internal/banks"
-	"fmt"
+	"PaymentsBot/internal/rncard"
+	"PaymentsBot/internal/scheduler"
+	"PaymentsBot/internal/tg"
 	"log"
 	"net/http"
 )
 
 func main() {
+	TgBotService, err := tg.NewTelegramService("8440241939:AAEvMsPT9FeOFWlvexZfvmxg9GcOxXoR7yE")
+
+	if err != nil {
+		log.Fatalf("error create tgbot %v", err)
+	}
+
+	banks.SetTelegram(TgBotService)
+	rncard.SetTelegram(TgBotService)
+
+	scheduler.SendDailyScheduler(rncard.FetchAndSendTransactions)
+
 	http.HandleFunc("/webhook", banks.TochkaBankHandler)
 	http.HandleFunc("/modulbank", banks.ModuleBankHandler)
 	http.HandleFunc("/tbank", banks.TBankHandler)
-	fmt.Println("Server started at :8080")
+	log.Println("Server started at :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
